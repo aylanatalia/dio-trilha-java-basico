e416b9dcb076a50fd90863a01d8ae2d5f8292d8f
Ayla Natalia Martins Ferreira <aylantalia08@hotmail.com>
2024-06-07 23:23:34 -0300

    enums-exemplo

--- exemplos-pacotes/src/com/mastersoft/clinical/ExameService.java
+++
@@ -1,8 +0,0 @@
-public class ExameService{
-    void salvarExame(Exame exame)package com.mastersoft.clinical;
-    
-    public class ExameService {
-        
-    }
-    
-}
--- exemplos-pacotes/src/com/mastersoft/clinical/model/Exame.java
+++
@@ -1,5 +0,0 @@
-public class Exame {
-   void exame() {
-    
-   }
-}
